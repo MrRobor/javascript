08610d24bfb9ffe4cb6efe615fb67ab93308de47
Akim <akimserverov@gmail.com>
2020-11-30 18:24:38 +0300

    commented

--- js/script.js
+++ js/script.js
@@ -1,28 +1,31 @@
 "use strict";
-// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
+// FRIST APP
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// const a = prompt('Один из последних просмотренных фильмов?', ''),
-//       b = prompt('На сколько оцените его?', ''),
-//       c = prompt('Один из последних просмотренных фильмов?', ''),
-//       d = prompt('На сколько оцените его?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;    
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-// console.log(personalMovieDB);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;    
 
+console.log(personalMovieDB);
 
-// let year = +prompt('Когда Аким родился');
-// if(year == 2002) {
-//     alert('Ты настоящий друг! Акима!');
-// } else {
-//     alert('Жаль, но ты не прошёл проверку.')
-// }
+// SECOND APP
+
+let year = +prompt('Когда Аким родился');
+if(year == 2002) {
+    alert('Ты настоящий друг! Акима!');
+} else {
+    alert('Жаль, но ты не прошёл проверку.')
+}
